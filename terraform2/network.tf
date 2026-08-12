f85a1228763391ad3f0538fd6c8176fcec3443bd
azin@LAPTOP-USV57JF3.localdomain <azinuddinazlani@gmail.com>
2026-08-13 07:40:20 +0800

    fix tf files and add ec2.tf with data blocks

--- terraform2/network.tf
+++ terraform2/network.tf
@@ -37,22 +37,3 @@ resource "aws_route_table_association" "my_link" {
   route_table_id = aws_route_table.my_route_table.id
 }
 
-resource "aws_security_group" "my_sg" {
-  name   = "tf-web-sg"
-  vpc_id = aws_vpc.my_vpc.id
-  tags = { Name = "tf-web-sg" }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "my_inbound_rule" {
-  security_group_id = aws_security_group.my_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "my_outbound_rule" {
-  security_group_id = aws_security_group.my_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
